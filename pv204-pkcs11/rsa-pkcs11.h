60600f8d96f1a7b9214032ded23dea5ddeb5c328
RichardKalinec <rkalinec@gmail.com>
2019-04-18 02:10:47 +0200

    Added encryption and decryption demo and main() function
    Functions making up the encryption and decryption demo are derived from the corresponding functions of the signing and verification demo.

--- pv204-pkcs11/rsa-pkcs11.h
+++ pv204-pkcs11/rsa-pkcs11.h
@@ -105,12 +105,18 @@ static CK_ULONG modulusbits = 1024;
 static CK_BYTE public_exponent[] = {3};
 
 /* Find a slot capable of:
- * . signing and verifying with sv_mech
+ * . signing and verifying with op_mech OR decrypting and encrypting with op_mech
  * . generating a key pair with kpgen_mech
  * Returns B_TRUE when successful. */
-boolean_t GetKGSVSlot(CK_MECHANISM_TYPE sv_mech, CK_MECHANISM_TYPE kpgen_mech,
-    CK_SLOT_ID_PTR pSlotID)
+boolean_t GetMySlot(CK_MECHANISM_TYPE op_mech, CK_MECHANISM_TYPE kpgen_mech,
+    CK_SLOT_ID_PTR pSlotID, int operations)
 {
+	if ((operations < 1) || (operations > 2))
+	{
+		printf("Invalid argument operations to GetMySlot()!n");
+		exit(1);
+	}
+	
 	CK_SLOT_ID_PTR pSlotList = NULL_PTR;
 	CK_SLOT_ID SlotID;
 	CK_ULONG ulSlotCount = 0;
@@ -149,14 +155,25 @@ boolean_t GetKGSVSlot(CK_MECHANISM_TYPE sv_mech, CK_MECHANISM_TYPE kpgen_mech,
 		/* Check if this slot is capable of signing and
 		 * verifying with sv_mech. */
 
-		if (m_pkcs11Mngr.C_GetMechanismInfo(SlotID, sv_mech, &mech_info); != CKR_OK) {
+		if (m_pkcs11Mngr.C_GetMechanismInfo(SlotID, op_mech, &mech_info); != CKR_OK) {
 			continue;
 		}
 
-		if (!(mech_info.flags & CKF_SIGN &&
-			mech_info.flags & CKF_VERIFY)) {
-			continue;
+		if (operations == 1)
+		{
+			if (!(mech_info.flags & CKF_SIGN &&
+				mech_info.flags & CKF_VERIFY)) {
+				continue;
+			}
 		}
+		esle if (operations == 2)
+		{
+			if (!(mech_info.flags & CKF_DECRYPT &&
+				mech_info.flags & CKF_ENCRYPT)) {
+				continue;
+			}
+		}
+		
 
 		/* Check if the slot is capable of key pair generation
 		 * with kpgen_mech. */
@@ -269,7 +286,7 @@ kgsvDemo(int argc, char **argv)
 
 	TS_ASSERT(m_pkcs11Mngr.Init(PKCS11_DLL) == CKR_OK);
 
-	found_slot = GetKGSVSlot(smech.mechanism, genmech.mechanism, &slotID);
+	found_slot = GetMySlot(smech.mechanism, genmech.mechanism, &slotID, 1);
 
 	if (!found_slot) {
 		fprintf(stderr, "No usable slot was found.\n");
@@ -352,4 +369,189 @@ kgsvDemo(int argc, char **argv)
 		publickey, message, messagelen, sign, &slen,
 		getattributes);
 	return;
+}
+
+void
+pkcs11_encrypt_decrypt_demo(CK_SESSION_HANDLE hSession, CK_MECHANISM* emech, CK_OBJECT_HANDLE privatekey,
+	CK_OBJECT_HANDLE publickey, uchar_t* message, CK_ULONG messagelen, char* ciphertext, CK_ULONG* clen,
+	CK_ATTRIBUTE* getattributes)
+{
+	TS_ASSERT(m_pkcs11Mngr.C_EncryptInit(hSession, emech, publickey) == CKR_OK);
+
+	TS_ASSERT(m_pkcs11Mngr.C_Encrypt(hSession, (CK_BYTE_PTR)message, messagelen,
+	    (CK_BYTE_PTR)ciphertext, clen) == CKR_OK);
+
+	fprintf(stdout, "Message was successfully encrypted with public key!\n");
+
+	TS_ASSERT(m_pkcs11Mngr.C_DecryptInit(hSession, emech, privatekey) == CKR_OK);
+
+	decrypted[BUFFERSIZ];
+	declen = BUFFERSIZ;
+	TS_ASSERT(m_pkcs11Mngr.C_Decrypt(hSession, (CK_BYTE_PTR)ciphertext, *clen,
+	    (CK_BYTE_PTR)decrypted, declen) == CKR_OK);
+	    
+	TS_ASSERT(strcmp(message, decrypted) == 0);
+
+	fprintf(stdout, "Message was successfully decrypted with private key!\n");
+	
+	// Close session
+	(void) m_pkcs11Mngr.C_CloseSession(hSession);
+	
+	// Perform final cleanup
+	(void) m_pkcs11Mngr.C_Finalize(NULL_PTR);
+
+	for (i = 0; i < template_size; i++) {
+		if (getattributes[i].pValue != NULL)
+			free(getattributes[i].pValue);
+	}
+}
+
+/* Example encrypts and decrypts a simple string, using a public/private
+ * key pair. */
+void
+edDemo(int argc, char **argv)
+{
+	CK_MECHANISM genmech, emech;
+	CK_SESSION_HANDLE hSession;
+	CK_SESSION_INFO sessInfo;
+	CK_SLOT_ID slotID;
+	int error, i = 0;
+
+	CK_OBJECT_HANDLE privatekey, publickey;
+
+    /* Set public key. */
+	CK_ATTRIBUTE publickey_template[] = {
+		{CKA_ENCRYPT, &truevalue, sizeof (truevalue)},
+		{CKA_MODULUS_BITS, &modulusbits, sizeof (modulusbits)},
+		{CKA_PUBLIC_EXPONENT, &public_exponent,
+		    sizeof (public_exponent)}
+	};
+
+    /* Set private key. */
+	CK_ATTRIBUTE privatekey_template[] = {
+		{CKA_DECRYPT, &truevalue, sizeof (truevalue)},
+		{CKA_TOKEN, &falsevalue, sizeof (falsevalue)},
+		{CKA_SENSITIVE, &truevalue, sizeof (truevalue)},
+		{CKA_EXTRACTABLE, &truevalue, sizeof (truevalue)}
+	};
+
+    /* Create sample message. */
+	CK_ATTRIBUTE getattributes[] = {
+		{CKA_MODULUS_BITS, NULL_PTR, 0},
+		{CKA_MODULUS, NULL_PTR, 0},
+		{CKA_PUBLIC_EXPONENT, NULL_PTR, 0}
+	};
+
+	CK_ULONG messagelen, template_size;
+
+	boolean_t found_slot = B_FALSE;
+	uchar_t *message = (uchar_t *)"Simple message for encrypting & decrypting.";
+	uchar_t *modulus, *pub_exponent;
+	char ciphertext[BUFFERSIZ];
+	clen = BUFFERSIZ;
+
+	messagelen = strlen((char *)message);
+
+	/* Set up mechanism for generating key pair */
+	genmech.mechanism = CKM_RSA_PKCS_KEY_PAIR_GEN;
+	genmech.pParameter = NULL_PTR;
+	genmech.ulParameterLen = 0;
+
+	/* Set up the signing mechanism */
+	emech.mechanism = CKM_RSA_PKCS;
+	emech.pParameter = NULL_PTR;
+	emech.ulParameterLen = 0;
+
+	TS_ASSERT(m_pkcs11Mngr.Init(PKCS11_DLL) == CKR_OK);
+
+	found_slot = GetMySlot(smech.mechanism, genmech.mechanism, &slotID, 2);
+
+	if (!found_slot) {
+		fprintf(stderr, "No usable slot was found.\n");
+		(void) m_pkcs11Mngr.C_Finalize(NULL_PTR);
+		exit(error);
+	}
+
+	fprintf(stdout, "selected slot: %d\n", slotID);
+
+	/* Open a session on the slot found */
+	TS_ASSERT(m_pkcs11Mngr.C_OpenSession(slotID, CKF_SERIAL_SESSION, NULL_PTR, NULL_PTR,
+	    &hSession) == CKR_OK);
+
+	// Login user
+	TS_ASSERT(m_pkcs11Mngr.C_Login(hSession, CKU_USER, (CK_CHAR *) TEST_PIN, strlen(TEST_PIN)) == CKR_OK);
+
+	fprintf(stdout, "Generating keypair....\n");
+
+	/* Generate Key pair for signing/verifying */
+	TS_ASSERT(m_pkcs11Mngr.C_GenerateKeyPair(hSession, &genmech, publickey_template,
+	    (sizeof (publickey_template) / sizeof (CK_ATTRIBUTE)),
+	    privatekey_template,
+	    (sizeof (privatekey_template) / sizeof (CK_ATTRIBUTE)),
+	    &publickey, &privatekey) == CKR_OK);
+
+	/* Display the publickey. */
+	template_size = sizeof (getattributes) / sizeof (CK_ATTRIBUTE);
+
+	if (m_pkcs11Mngr.C_GetAttributeValue(hSession, publickey, getattributes,
+	    template_size) != CKR_OK) {
+		/* not fatal, we can still decrypt/encrypt if this failed */
+		fprintf(stderr, "C_GetAttributeValue: rv = 0x%.8X\n", rv);
+		error = 1;
+	} else {
+		/* Allocate memory to hold the data we want */
+		for (i = 0; i < template_size; i++) {
+			getattributes[i].pValue = 
+			    malloc (getattributes[i].ulValueLen * 
+				sizeof(CK_VOID_PTR));
+			if (getattributes[i].pValue == NULL) {
+				int j;
+				for (j = 0; j < i; j++)
+					free(getattributes[j].pValue);
+				pkcs11_encrypt_decrypt_demo(hSession, &emech, privatekey,
+					publickey, message, messagelen, ciphertext, &clen,
+					getattributes);
+				return;
+			}
+		}
+
+		/* Call again to get actual attributes */
+		if (m_pkcs11Mngr.C_GetAttributeValue(hSession, publickey, getattributes,
+	    template_size) != CKR_OK) {
+			/* not fatal, we can still sign/verify if failed */
+			fprintf(stderr,
+			    "C_GetAttributeValue: rv = 0x%.8X\n", rv);
+			error = 1;
+		} else {
+			/* Display public key values */
+			fprintf(stdout, "Public Key data:\n\tModulus bits: "
+			    "%d\n", 
+			    *((CK_ULONG_PTR)(getattributes[0].pValue)));
+
+			fprintf(stdout, "\tModulus: ");
+			modulus = (uchar_t *)getattributes[1].pValue;
+			for (i = 0; i < getattributes[1].ulValueLen; i++) {
+				fprintf(stdout, "%.2x", modulus[i]);
+			}
+
+			fprintf(stdout, "\n\tPublic Exponent: ");
+			pub_exponent = (uchar_t *)getattributes[2].pValue;
+			for (i = 0; i< getattributes[2].ulValueLen; i++) {
+				fprintf(stdout, "%.2x", pub_exponent[i]);
+			}
+			fprintf(stdout, "\n");
+		}
+	}
+	
+	pkcs11_encrypt_decrypt_demo(hSession, &emech, privatekey,
+		publickey, message, messagelen, ciphertext, &clen,
+		getattributes);
+	return;
+}
+
+int
+main(int argc, char** argv)
+{
+	kgsvDemo();
+	edDemo();
 }
